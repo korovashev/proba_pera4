6bec0f0a4079ac7c5575cf58f13293a844aef505
korovashev <dima.korovashev@mail.ru>
2023-03-16 22:08:57 +0300

    lab4

--- lab4_dop.py
+++ lab4_dop.py
@@ -4,7 +4,7 @@ import wave
 filename = input("Введите путь к файлу .wav: ")
 
 if not os.path.isfile(filename):
-    print(f"Файл {filename} не найден.")
+    print(f"Файл {filename} не найден!")
     exit()
 
 rate_change = float(input("Введите коэффициент изменения скорости (> 0): "))
@@ -22,4 +22,4 @@ with wave.open(filename, 'rb') as wav_file:
         new_wav_file.setparams((params[0], params[1], new_rate, params[3], params[4], params[5]))
         frames = wav_file.readframes(params[3])
         new_wav_file.writeframes(frames)
-        print(f"Файл {new_filename} создан.")
+        print(f"Файл {new_filename} создан!")
